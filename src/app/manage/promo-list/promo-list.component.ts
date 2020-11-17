c91458a30f3e24bd1d43630167db5129328eec33
palangue <jwcho79@gmail.com>
2020-11-17 21:26:56 +0900

    delete console log

--- src/app/manage/promo-list/promo-list.component.ts
+++ src/app/manage/promo-list/promo-list.component.ts
@@ -14,8 +14,8 @@ import { take } from 'rxjs/operators';
   styleUrls: ['./promo-list.component.css'],
   animations: [
     trigger('promotion_details', [
-      state('collapsed', style({ height: '0px', minHeight: '0'})),
-      state('expanded', style({height : '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)'))
     ])
   ] // End Animations
@@ -23,60 +23,46 @@ import { take } from 'rxjs/operators';
 export class PromoListComponent implements OnInit {
 
   displayedColumns: string[] = ['coName', 'coCode', 'cost', 'start', 'end'];
-  promotionDataSource : Promotion[];
+  promotionDataSource: Promotion[];
 
-  expandedElement : SupportPromotionDevice | null;
-  promotionDeviceColumns : string [] = ['name', 'price', 'promotion_actions'];
+  expandedElement: SupportPromotionDevice | null;
+  promotionDeviceColumns: string[] = ['name', 'price', 'promotion_actions'];
 
-  promo_sub : Subscription;
+  promo_sub: Subscription;
 
   constructor(
-    public dialog : MatDialog,
-    private deviceService : DeviceService
+    public dialog: MatDialog,
+    private deviceService: DeviceService
   ) { }
 
   ngOnInit(): void {
-    this.promo_sub = this.deviceService.getPromotion('promotion').valueChanges({idField : 'idx'}).pipe(take(1)).subscribe( (ref : Promotion[]) => {
+    this.promo_sub = this.deviceService.getPromotion('promotion').valueChanges({ idField: 'idx' }).pipe(take(1)).subscribe((ref: Promotion[]) => {
 
-      console.log(11111111);
-      
-      ref.forEach((item) =>{
-        console.log(22222);
+      ref.forEach((item) => {
         this.deviceService.getPromotion('promotion').doc(item.idx).collection('support_promo').valueChanges().pipe(take(1)).subscribe(
           ref2 => {
-            console.log(333333);
             item.support = ref2;
-            //this.promotionDataSource = ref;
-            
-          }
-          
-      );
-      console.log(44444);
+          });
       });
-
-      console.log(55555);
       this.promotionDataSource = ref;
-      
     });
-
-    
   }
 
   //#region 행사 추가 다이얼로그
-  btnAddDevice(){
-    const dialogRef =  this.dialog.open(PromoDetailComponent, {
-      data : { width : '400px', data : false}
+  btnAddDevice() {
+    const dialogRef = this.dialog.open(PromoDetailComponent, {
+      data: { width: '400px', data: false }
     });
-    dialogRef.afterClosed().subscribe( result =>{
-      if(result == true) alert("저장 성공");
+    dialogRef.afterClosed().subscribe(result => {
+      if (result == true) alert("저장 성공");
     });
   }
   //#endregion
 
-  promo_item_modify(data, data2){
+  promo_item_modify(data, data2) {
     console.log(data, data2);
   }
-  promo_item_delete(data){
+  promo_item_delete(data) {
     console.log(data);
   }
 }
