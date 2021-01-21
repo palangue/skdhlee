3e5b72d0fb847ec38c947c7f21287573f54c4da7
palangue <jwcho79@gmail.com>
2021-01-21 22:16:17 +0900

    delete log

--- src/app/utls/upload-ctrl/upload-ctrl.component.ts
+++ src/app/utls/upload-ctrl/upload-ctrl.component.ts
@@ -56,34 +56,20 @@ export class UploadCtrlComponent implements OnInit, OnDestroy {
       // tap(console.log),
       finalize(async () => {
         this.downloadUrl = await ref.getDownloadURL().toPromise();
-
-        console.log('download url = ', this.downloadUrl, path);
-        console.log('deviceName = ', this.deviceName);
         this.db.collection<PHONE_DETAIL>('Phone', ref => ref.where('ModelName', '==', this.deviceName))
           .stateChanges().pipe(take(1), map(result => {
             return result.map(resultData => {
               const idx = resultData.payload.doc.id;
               const data = resultData.payload.doc.data();
-
-              console.log(idx,data);
-
               return { idx, ...data };
             });
           })).subscribe(response => {
-            
-            console.log(response);
-            
+          
             if (response.length > 0) {
               if (this.IsPrimary) {
-
-                console.log(this.IsPrimary);
-
                 for (let item of response) {
-                  console.log(item.idx, item.mainImgSrc);
-
                   item.mainImgSrc = this.downloadUrl;
                   this.db.collection<PHONE_DETAIL>('Phone').doc(item.idx).update(item)
-                    .then(() => alert('성공'))
                     .catch((err) => alert(err));
                 }
               }
